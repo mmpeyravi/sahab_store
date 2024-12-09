06b138c83dc20ba4181c9dcb74add4c7272797cc
mmpeyravi <mmpeyravi@gmail.com>
2024-12-09 17:07:22 +0330

    clean code

--- my-store/src/components/Layout.tsx
+++ my-store/src/components/Layout.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link, Outlet} from "react-router-dom";
+import {Product} from "../types";
 interface LayoutProps {
 }
 const Layout: React.FC<LayoutProps> = () => {
+    const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
 
 
     return (
         <div className="my-app">
-            <Outlet/>
+            <img style={{marginTop: '10px'}} src={require('../../src/icons/sahab store logo.png')} width={250}/>
+
+            <Outlet />
 
             <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
             <div className="floating-container">
                 <Link to="/cart">
-                    <div className="floating-button">+</div>
+                    <div className="floating-button">
+                        <img style={{ marginTop: '10px'}} src={require('../../src/icons/icons8-cart-94.png')} width={40} />
+                    </div>
                 </Link>
                 {/*<div className="element-container">
 
--- my-store/src/index.tsx
+++ my-store/src/index.tsx
@@ -1,44 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom';
 import './styles.css';
-import Cart from './components/Cart';
-import {Product} from "./types";
-import axios from "axios";
-import Filter from "./components/Filter";
-import ProductList from "./components/ProductList";
-import ProductDetail from "./components/ProductDetail";
 import App from "./App";
-// import Home from './pages/Home';
-// import Blogs from './pages/Blogs';
-// import Contact from './pages/Contact';
-// import NoPage from './pages/NoPage';
-
-// مشخص کردن نوع Props در صورت لزوم
-
-
-
-
-interface BlogsProps {
-}
-
-interface ContactProps {
-}
-
-interface NoPageProps {
-}
-
-
-
-// const Blogs: React.FC<BlogsProps> = () => {/* پیاده‌سازی کامپوننت Blogs */}
-// const Contact: React.FC<ContactProps> = () => {/* پیاده‌سازی کامپوننت Contact */}
-// const NoPage: React.FC<NoPageProps> = () => {/* پیاده‌سازی کامپوننت NoPage */}
-
-/*const App: React.FC = () => {
-    return (
-
-    );
-};*/
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
